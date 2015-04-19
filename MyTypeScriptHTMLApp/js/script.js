2b36bea34fb6b6d050bf50546fa4518789c4113c
danilbrenner <danilbrenner@outlook.com>
2015-04-19 17:42:37 +0200

    Init

---
+++ MyTypeScriptHTMLApp/js/script.js
@@ -0,0 +1,83 @@
+﻿
+var data;
+var viewedData;
+var sortBy = '';
+var sortDirection = '';
+
+function filter(filterStr) {
+    filterStr = filterStr.toLowerCase();
+    viewedData = data.filter(function (val) {
+        return val.name.toLowerCase().indexOf(filterStr) === 0;
+    });
+    showItems(viewedData);
+};
+
+function appendTd(nodeTr, value) {
+    var nodeTd = document.createElement("TD");
+    var textnode = document.createTextNode(value);
+    nodeTd.appendChild(textnode);
+    nodeTr.appendChild(nodeTd);
+}
+
+function showItems(items) {
+    var pList = document.getElementById('person-table').getElementsByTagName('tbody')[0];
+    pList.innerHTML = '';
+    items.forEach(function (val) {
+        var nodeTr = document.createElement("TR");
+        appendTd(nodeTr, val.name);
+        appendTd(nodeTr, val.age);
+        pList.appendChild(nodeTr);
+    });
+}
+
+function sortByName(a, b) {
+    if (a.name > b.name) {
+        return sortDirection === 'asc' ? 1 : -1;
+    }
+    if (a.name < b.name) {
+        return sortDirection === 'asc' ? -1 : 1;
+    }
+    return 0;
+}
+
+function sortByAge(a, b) {
+    if (a.age > b.age) {
+        return sortDirection === 'asc' ? 1 : -1;
+    }
+    if (a.age < b.age) {
+        return sortDirection === 'asc' ? -1 : 1;
+    }
+    return 0;
+}
+
+function getSortFunction() {
+    if (sortBy === 'fullName') return sortByName;
+    if (sortBy === 'age') return sortByAge;
+}
+
+function resortBy(field) {
+    if (sortBy !== field) {
+        sortBy = field;
+        sortDirection = 'asc';
+    } else sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+    var func = getSortFunction();
+    if (!func) return;
+    viewedData = viewedData.sort(func);
+    showItems(viewedData);
+};
+
+window.onload = function () {
+    data = dataProvider.getPersonData();
+    viewedData = data;
+    showItems(data);
+    document.getElementById('filter-button').onclick = function () {
+        var filterStr = document.getElementById('filter-input').value;
+        filter(filterStr);
+    };
+    document.getElementById('full-name-head').onclick = function () {
+        resortBy('fullName');
+    };
+    document.getElementById('age-head').onclick = function () {
+        resortBy('age');
+    };
+};
---
+++ MyTypeScriptHTMLApp/one.js
@@ -0,0 +1,39 @@
+﻿var isActive = false;
+var size = 8;
+var name = "Вася";
+
+var list = [1, 2, 3];
+var list = [1, 2, 3];
+
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = 1 /* Green */;
+
+var notSure = 4;
+notSure = "maybe a string instead";
+notSure = false; // okay, definitely a boolean
+
+function sayHi() {
+    console.log("Hi");
+}
+
+var one;
+one = {
+    firstName: "",
+    lastName: "",
+    age: 10,
+    sayHi: function () {
+        console.log("Hello " + this.firstName);
+    }
+};
+
+var ff;
+ff = function () {
+    return "";
+};
+//# sourceMappingURL=one.js.map
